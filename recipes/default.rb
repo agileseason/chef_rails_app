1f55ad899edc8ef1c26cfd4398b11d1f838ab1d0
Andrey Sidorov <takandar@gmail.com>
2017-09-26 14:07:01 +0300

    add conditions

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,22 +5,26 @@ app.node['dirs'].each do |key, dir|
   mkdir dir
 end
 
-chef_rails_app_secrets_yml "#{app.dir :shared}/config/secrets.yml" do
-  app_env app.env
-  app_user app.user
-  app_group app.group
-  secrets app.node['secrets']
+if app.node['secrets']
+  chef_rails_app_secrets_yml "#{app.dir :shared}/config/secrets.yml" do
+    app_env app.env
+    app_user app.user
+    app_group app.group
+    secrets app.node['secrets']
+  end
 end
 
-chef_rails_app_database_yml "#{app.dir :shared}/config/database.yml" do
-  app_env app.env
-  app_user app.user
-  app_group app.group
-  database "#{app.name}_#{app.env}"
+if node['postgresql']
+  chef_rails_app_database_yml "#{app.dir :shared}/config/database.yml" do
+    app_env app.env
+    app_user app.user
+    app_group app.group
+    database "#{app.name}_#{app.env}"
 
-  credentials = node['postgresql']['users']
-    .find { |v| v['username'] == database }
+    credentials = node['postgresql']['users']
+      .find { |v| v['username'] == database }
 
-  db_user credentials['username']
-  db_password credentials['password']
+    db_user credentials['username']
+    db_password credentials['password']
+  end
 end
